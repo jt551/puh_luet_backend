e2ee7d1a05ed01c2470d24909ebe7292befbcf5f
jt551 <45320525+jt551@users.noreply.github.com>
2020-01-01 19:47:52 +0200

    pre-heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 const bodyParser = require('body-parser')
@@ -9,6 +10,7 @@ morgan.token('bodytiedot', function (req) {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodytiedot'))
+app.use(cors())
 
 let persons = [
     { 
@@ -90,7 +92,7 @@ app.get('/api/persons', (request, response) => {
     response.json(newPerson)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
