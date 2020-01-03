4e29e23f341be5b0e075000aab497b15dae36cd8
jt551 <45320525+jt551@users.noreply.github.com>
2020-01-03 16:13:16 +0200

    complete 3.19 3.20backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+
 const Person = require('./models/person')
 
 const app = express()
@@ -15,28 +16,6 @@ morgan.token('bodytiedot', function (req) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodytiedot'))
 app.use(cors())
 
-let persons = [
-    { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-64231221",
-        "id": 4
-      }
-];
 
 app.get('/', (req, res) => {
   res.send('<h1>Puhelinluettelo</h1>')
@@ -76,36 +55,23 @@ app.get('/api/persons', (request, response, next) => {
     .catch(error => next(error))
   })
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
     const id = Math.floor(Math.random() * 100000000);  
     const newName = request.body.name;
     const newNumber = request.body.number;
-    if(!newName || !newNumber){
-      console.log('POST name, number ',!newName, !newNumber);
-      
-      return response.status(400).json({
-        error: 'Name and/or Number missing'
-      })
-    }
-    //newPerson.id = id;
-    //console.log(newPerson);
 
-    /*const haku = persons.find(p => p.name === newName)
-    if(haku){
-      //console.log("if hakutulos: ", haku);
-      
-      return response.status(400).json({
-        error: 'Name must be unique'
-      })
-    }*/
     const person = new Person({
       name: newName,
       number: newNumber
     })
   
-    person.save().then(savedPerson => {
-      response.json(savedPerson.toJSON())
+    person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
     })
+    .catch(error => next(error))
     
   })
 
@@ -135,12 +101,13 @@ app.get('/api/persons', (request, response, next) => {
   
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }   
     next(error)
   }
-  
   app.use(errorHandler)
+  
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
+mongoose.set('useFindAndModify', false);
 
 console.log('connecting to', url)
 
@@ -13,9 +15,10 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: { type: String,  minlength: 3, required: true, unique: true },
+  number: { type: String,  minlength: 8, required: true }
 })
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
